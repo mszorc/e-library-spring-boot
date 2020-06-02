04d4b91958aa0519c49ce9a314642a40ccb09ce1
Adam Sulima Dolina <adam.sulimadolina@gmail.com>
2020-06-02 21:36:38 +0200

    Fixes

--- src/main/java/com/example/elibrary/ElibraryApplication.java
+++ src/main/java/com/example/elibrary/ElibraryApplication.java
@@ -1,15 +1,9 @@
 package com.example.elibrary;
 
-//import com.example.elibrary.auth.JwtFilter;
-import io.jsonwebtoken.Jwt;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
-
-import java.util.Collections;
 
 
 @SpringBootApplication
@@ -18,13 +12,5 @@ public class ElibraryApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElibraryApplication.class, args);
     }
-
-//    @Bean
-//    public FilterRegistrationBean filterRegistrationBean() {
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(new JwtFilter());
-//        filterRegistrationBean.setUrlPatterns(Collections.singleton("/api/*"));
-//        return filterRegistrationBean;
-//    }
 
 }
